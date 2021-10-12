d64695b47d8f85787f8e8bad20bd88443bb35d38
JCervantesB <pc3dtepic@gmail.com>
2021-10-11 18:01:33 -0600

    Iniciado el router publico

---
+++ controllers/PaginasController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+class PaginasController {
+    public static function index() {
+        echo "Desde Index";
+    }
+    public static function nosotros() {
+        echo "Desde Nosotros";
+    }
+    public static function propiedades() {
+        echo "Desde Propiedades";
+    }
+    public static function propiedad() {
+        echo "Desde Propiedades";
+    }
+    public static function blog() {
+        echo "Desde Blog";
+    }
+    public static function entrada() {
+        echo "Desde Entrada";
+    }
+    public static function contacto() {
+        echo "Desde Contacto";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,9 +4,10 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\PropiedadController;
 use Controllers\VendedorController;
+use Controllers\PaginasController;
 
 $router = new Router();
-
+// Zona privada
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->post('/propiedades/crear', [PropiedadController::class, 'crear']);
@@ -19,5 +20,15 @@ $router->post('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
+
+// Zona publica
+$router->get('/', [PaginasController::class, 'index']);
+$router->get('/nosotros', [PaginasController::class, 'nosotros']);
+$router->get('/propiedades', [PaginasController::class, 'propiedades']);
+$router->get('/propiedad', [PaginasController::class, 'propiedad']);
+$router->get('/blog', [PaginasController::class, 'blog']);
+$router->get('/entrada', [PaginasController::class, 'entrada']);
+$router->get('/contacto', [PaginasController::class, 'contacto']);
+$router->post('/contacto', [PaginasController::class, 'contacto']);
 
 $router->comprobarRutas();
