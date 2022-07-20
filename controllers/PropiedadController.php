abf933fb5d123cae39f2472be3408cfe7a48df7a
imjcervantes <imjcervantes@gmail.com>
2022-07-20 09:20:25 -0600

    test

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -55,7 +55,8 @@ class PropiedadController {
                 }
                 //Guarda la imagen en el servidor
                 $image->save(CARPETA_IMAGENES . $nombreImagen);
-    
+                
+                debuguear($propiedad);
                 //Guarda en la base de datos
                 $propiedad->guardar();
                 
