1fb12ec095132d8e047bc14dd0c0afa0d73c0120
imjcervantes <imjcervantes@gmail.com>
2022-07-20 08:46:13 -0600

    preparando deployment

---
+++ DataBase/bienes_raices.sql
@@ -0,0 +1,87 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `blog` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `imagen` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `descripcion` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `contenido` longtext CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `creado` date DEFAULT NULL,
+  `usuarioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuarioId_idx` (`usuarioId`),
+  CONSTRAINT `usuarioId` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+
+CREATE TABLE `cliente` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(60) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+CREATE TABLE `propiedades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(60) DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  `imagen` varchar(200) DEFAULT NULL,
+  `descripcion` longtext,
+  `habitaciones` int DEFAULT NULL,
+  `wc` int DEFAULT NULL,
+  `estacionamiento` int DEFAULT NULL,
+  `creado` date DEFAULT NULL,
+  `vendedorId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `vendedorId_idx` (`vendedorId`),
+  CONSTRAINT `vendedorId` FOREIGN KEY (`vendedorId`) REFERENCES `vendedores` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` char(60) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `vendedores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+
+
+
+
+
+INSERT INTO `propiedades` (`id`, `titulo`, `precio`, `imagen`, `descripcion`, `habitaciones`, `wc`, `estacionamiento`, `creado`, `vendedorId`) VALUES
+(9, 'Test', 321654.00, 'c8b336e9101a53e98a05beb5dd982a65.jpg', 'Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test ', 2, 1, 1, '2022-06-28', 3);
+
+
+INSERT INTO `usuarios` (`id`, `email`, `password`, `nombre`) VALUES
+(1, 'correo@correo.com', '$2y$10$8vLUO2Merb4zm0wQ/BjvGejadGAKbwRnLa.KI/4aB5i3GQuCtojmG', 'Admin');
+
+
+INSERT INTO `vendedores` (`id`, `nombre`, `apellido`, `telefono`) VALUES
+(3, 'Julio', 'Cervantes', '3126549870');
+INSERT INTO `vendedores` (`id`, `nombre`, `apellido`, `telefono`) VALUES
+(4, 'Juan', 'Test', '3215649870');
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- includes/config/database.php
+++ includes/config/database.php
@@ -1,20 +1,18 @@
 <?php
 
-$db = mysqli_connect(
-    $_ENV['DB_HOST'], 
-    $_ENV['DB_USER'], 
-    $_ENV['DB_PASS'], 
-    $_ENV['DB_DBNAME']
-);
-    
-$db->set_charset("utf8");
+function conectarDB() : mysqli {
+    $db = new mysqli(
+        $_ENV['DB_HOST'], 
+        $_ENV['DB_USER'], 
+        $_ENV['DB_PASS'], 
+        $_ENV['DB_DBNAME']
+    );
+    $db->set_charset("utf8");
 
-if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
-    exit;
+    if(!$db) {
+        echo "Error no se pudo conectar";
+        exit;
+    } 
+
+    return $db;
 }
-
-/* cambiar el conjunto de caracteres a utf8 */
-
